f5bbe44426ac6e47fab6cf664d54f5b14f1651cd
programishka <dev@programishka.ru>
2020-11-11 23:49:42 +0300

    add base operation

---
+++ counter.cpp
@@ -0,0 +1,14 @@
+#include "counter.h"
+
+Counter::Counter(): QObject(), m_nValue(0)
+{
+
+}
+
+void Counter::slotInc()
+{
+    emit counterChanged(++m_nValue);
+    if (m_nValue == 5){
+        emit goodbye();
+    }
+}
---
+++ counter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <QObject>
+
+class Counter: public QObject{
+    Q_OBJECT
+private:
+    int m_nValue;
+
+public:
+    Counter();
+
+public slots:
+    void slotInc();
+signals:
+    void goodbye();
+    void counterChanged(int);
+
+
+};
